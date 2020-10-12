3df3e0b579d0c15a40c4ae308f406b732060b527
kurosujiomake <kurosujiomake@gmail.com>
2020-10-11 17:48:07 -0700

    trying to learn the new player input

---
+++ Slimo2/Assets/Scripts/NewFinalizedVers/PlayerControlManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControlManager : MonoBehaviour
+{
+    public enum Direction
+    {
+        neutral,
+        up,
+        down,
+        left,
+        right
+    }
+    public Direction curStickDir;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
