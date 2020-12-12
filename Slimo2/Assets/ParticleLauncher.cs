fd8cf836afbeea7fa9998fb4002fcc293f63fb3c
Solstar01 <corbin.poynter@outlook.com>
2020-12-12 00:23:44 -0800

    effects
    created sword ult effect (still needs custom shaped sprite); added effects object to player (currently does not function but is not broken)

---
+++ Slimo2/Assets/ParticleLauncher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleLauncher : MonoBehaviour
+{
+    public ParticleSystem particleLauncher;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        particleLauncher.Emit(0);
+    }
+
+    public void PrintEvent(string s)
+    {
+        Debug.Log("Particles!");
+    }
+
+}
