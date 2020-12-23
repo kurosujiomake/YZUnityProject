96d77c950e7c85e4030b2cf7de845d3584366b4a
kurosujiomake <kurosujiomake@gmail.com>
2020-12-22 21:12:01 -0800

    combo things

--- Slimo2/Assets/Scripts/NewFinalizedVers/DamageNum/DamageNumberMain.cs
+++ Slimo2/Assets/Scripts/NewFinalizedVers/DamageNum/DamageNumberMain.cs
@@ -8,7 +8,8 @@ public class DamageNumberMain : MonoBehaviour
     public float Duration;
     public float dmgTest;
     public DmgText[] DisplayDmgText = new DmgText[5];
-    
+    [SerializeField]
+    private float totalDmg;
 
     void Start()
     {
@@ -19,7 +20,29 @@ public class DamageNumberMain : MonoBehaviour
         DisplayDmgText[0].Activate();
         DisplayDmgText[0].TxtObj.text = Mathf.RoundToInt(dmg).ToString() + DisplayDmgText[0].DisplayText;
     }
+    public void TotalDmgUpdate(float input)
+    {
+        DisplayDmgText[1].Activate();
+        DisplayDmgText[1].TxtObj.text = Mathf.RoundToInt(input).ToString() + DisplayDmgText[0].DisplayText;
+    }
+    public void AddTotalDmg(float input)
+    {
+        totalDmg += input;
 
+    }
+    public void ResetTotalDmg()
+    {
+        DisplayDmgText[1].Deactivate();
+        totalDmg = 0;
+
+    }
+
+    IEnumerator ComboTimer()
+    {
+        yield return new WaitForSeconds(Duration);
+        DeactivateAllText();
+        ResetTotalDmg();
+    }
     void Update()
     {
         HitDamageUpdate(dmgTest);
