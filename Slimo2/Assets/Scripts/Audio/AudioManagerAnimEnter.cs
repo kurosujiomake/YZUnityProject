f9444ca772cc6e575f1a767af9089475a91f039a
Solstar01 <corbin.poynter@outlook.com>
2021-01-21 00:41:10 -0800

    sound
    bool exits in animation window now turn off at appropriate times

--- Slimo2/Assets/Scripts/Audio/AudioManagerAnimEnter.cs
+++ Slimo2/Assets/Scripts/Audio/AudioManagerAnimEnter.cs
@@ -36,6 +36,7 @@ public class AudioManagerAnimEnter : StateMachineBehaviour
         audioManager.PlaySound(soundName);
     }
 
+    /*
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -44,6 +45,7 @@ public class AudioManagerAnimEnter : StateMachineBehaviour
             Debug.Log("OnStateUpdate Animator got updated!");
         }
     }
+    */
    
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
