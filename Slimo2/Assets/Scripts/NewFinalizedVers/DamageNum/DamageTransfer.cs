af0369df3965524706e447d8a536e84de3e05555
kurosujiomake <kurosujiomake@gmail.com>
2021-01-25 18:47:50 -0800

    decoupled dmg from knockback

---
+++ Slimo2/Assets/Scripts/NewFinalizedVers/DamageNum/DamageTransfer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTransfer : MonoBehaviour
+{
+    
+}
+[System.Serializable]
+public class DataBundle
+{
+    [SerializeField]
+    private float InputDmg, critChance;
+    [SerializeField]
+    private int Ele;
+
+    public float ReturnFloatValues(string which)
+    {
+        float a = 0;
+        switch(which)
+        {
+            case "dmg":
+                a = InputDmg;
+                break;
+            case "crit":
+                a = critChance;
+                break;
+        }
+        return a;
+    }
+    public int ReturnIntValues(string which)
+    {
+        int a = 0;
+        switch(which)
+        {
+            case "ele":
+                a = Ele;
+                break;
+        }
+        return a;
+    }
+    public void SetValues(float dmg, float crit, int ele)
+    {
+        InputDmg = dmg;
+        critChance = crit;
+        Ele = ele;
+    }
+}
