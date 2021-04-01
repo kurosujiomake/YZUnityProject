bc4407c7bd5f91ef6ac67be9dace6d3a52016b88
Solstar01 <corbin.poynter@outlook.com>
2021-04-01 00:49:32 -0700

    effects
    projectile effect added for ranged enemy

---
+++ Slimo2/Assets/Animations/Effects/efx_lightDimZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class efx_lightDimZone : MonoBehaviour
+{
+    //public GameObject lightZones;
+    Light myLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+        Debug.Log("found it");
+    }
+
+    void Update()
+    {
+        myLight.intensity = Mathf.PingPong(Time.time, 8);
+    }
+
+}
