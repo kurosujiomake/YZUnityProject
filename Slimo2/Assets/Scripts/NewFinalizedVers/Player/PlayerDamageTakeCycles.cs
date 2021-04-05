41a8273071a89d1282aeff601a959cb719e26ec2
kurosujiomake <kurosujiomake@gmail.com>
2021-04-04 21:26:45 -0700

    player KB extremely buggy, disabled for now

--- Slimo2/Assets/Scripts/NewFinalizedVers/Player/PlayerDamageTakeCycles.cs
+++ Slimo2/Assets/Scripts/NewFinalizedVers/Player/PlayerDamageTakeCycles.cs
@@ -7,6 +7,8 @@ public class PlayerDamageTakeCycles : MonoBehaviour
     public DamageReciever DR = null;
     public PlayerControllerNew pCN = null;
     public Animator anim = null;
+    public Rigidbody2D rb2d = null;
+    public KBDatabase kbD = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,7 @@ public class PlayerDamageTakeCycles : MonoBehaviour
         {
             anim = GetComponent<Animator>();
         }
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -43,5 +46,23 @@ public class PlayerDamageTakeCycles : MonoBehaviour
                 anim.SetTrigger("GotHit");
             }
         }
+    }
+    IEnumerator PKB(int KB_ID)
+    {
+        StopMovement();
+        PKnockback(kbD.Dir(KB_ID), kbD.Vel(KB_ID));
+        yield return new WaitForSeconds(kbD.KBDur(KB_ID));
+        StopMovement();
+    }
+    private void StopMovement()
+    {
+        rb2d.velocity = Vector2.zero;
+    }
+    void PKnockback(float dir, float force)
+    {
+        float d = dir * Mathf.Deg2Rad;
+        float f = force * Time.deltaTime;
+        Vector2 v = new Vector2(Mathf.Cos(d) * f, Mathf.Sin(d) * f);
+        rb2d.velocity = v;
     }
 }
