7e2f095786cf6b54cc9ec1365788889ae991b9bf
kurosujiomake <kurosujiomake@gmail.com>
2021-04-15 20:46:52 -0700

    spawner can basic spawn now
    and fixed a hp bar issue

--- Slimo2/Assets/Scripts/NewFinalizedVers/Enemy/EnemySpawner.cs
+++ Slimo2/Assets/Scripts/NewFinalizedVers/Enemy/EnemySpawner.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public EnemyPrefabDatabase eDB;
-    public EnemiesBundle[] eBundle;
+    public EnemiesBundle eBundle;
+    public bool startSpawn = false;
+    private bool hasSpawned = false;
     void Start()
     {
         
@@ -15,6 +17,39 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         
+        switch(eBundle.spType)
+        {
+            case SpawnType.none: //doesnt spawn anything, a failsafe just in case
+                break;
+            case SpawnType.single:
+                if(startSpawn && !hasSpawned)
+                {
+                    SpawnSingle();
+                }
+                break;
+            case SpawnType.continous:
+
+                break;
+            case SpawnType.restock:
+
+                break;
+        }
+    }
+
+    void SpawnSingle()
+    {
+        if(eBundle.SpawnPoints.Length > 1) //this signifies if it spawns multiple at once
+        {
+            for (int a = 0; a < eBundle.SpawnPoints.Length; a++)
+            {
+                Instantiate(eDB.ReturnPrefab(eBundle.EnemyIDs[0]), eBundle.SpawnPoints[a].position, Quaternion.identity);
+            }
+        }
+        if(eBundle.SpawnPoints.Length == 1)
+        {
+            Instantiate(eDB.ReturnPrefab(eBundle.EnemyIDs[0]), eBundle.SpawnPoints[0].position, Quaternion.identity);
+        }
+        hasSpawned = true;
     }
 }
 [System.Serializable]
@@ -30,7 +65,8 @@ public class EnemiesBundle
 
 public enum SpawnType
 {
-    single,
-    continous,
+    single, //single spawns onces and the deactivates
+    continous, //continous spawns after a certain duration until turned off 
+    restock, //restock spawns when the last mob(s) it spawned is killed
     none
 }
