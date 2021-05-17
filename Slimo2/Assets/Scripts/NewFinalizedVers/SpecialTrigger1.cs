93656e96b635d77f49552e0f5099b9283eba55b3
kurosujiomake <kurosujiomake@gmail.com>
2021-05-16 19:00:36 -0700

    colliders to give player/take away controls is done

---
+++ Slimo2/Assets/Scripts/NewFinalizedVers/SpecialTrigger1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialTrigger1 : MonoBehaviour
+{
+    [Header("This one turns on/off air dashes and attack")]
+    public bool TurnOn = false;
+    // Start is called before the first frame update
+    
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if(c.GetComponent<Parameters>() != null) //only the player should have this script attached
+        {
+            var v = c.GetComponent<Parameters>();
+            var a = c.GetComponent<Animator>();
+            switch(TurnOn)
+            {
+                case false:
+                    v.m_DashMax = 0;
+                    a.SetBool("IsGamePlay", false);
+                    break;
+                case true:
+                    v.m_DashMax = 5;
+                    a.SetBool("IsGamePlay", true);
+                    break;
+            }
+        }
+    }
+}
