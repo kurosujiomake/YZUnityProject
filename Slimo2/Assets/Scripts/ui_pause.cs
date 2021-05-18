d968ea1013e24306740cd4cec7c26afea8666fd0
Solstar01 <corbin.poynter@outlook.com>
2021-05-18 04:32:22 -0700

    ui
    ESC brings up a menu featuring both controller and keyboard button layouts.

---
+++ Slimo2/Assets/Scripts/ui_pause.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* allows players to bring up a menu displaying all the controls. 
+ * Does not pause all functions in the game.
+*/
+public class ui_pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void PauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            pauseMenu.SetActive(true);
+            AudioListener.pause = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+            AudioListener.pause = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            PauseGame();
+        }
+    }
+}
